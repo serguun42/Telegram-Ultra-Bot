c7e8305f8f37a9496110dc56c544be8c3476e05b
serguun42 <s42@serguun42.ru>
2022-12-23 20:11:43 +0300

    Version 3.4.2 – Updated logging

--- util/log.js
+++ util/log.js
@@ -1,9 +1,20 @@
 /* eslint-disable no-console */
 import { writeFile } from 'fs/promises';
-import DEV from './is-dev.js';
+import { inspect } from 'util';
+import IS_DEV from './is-dev.js';
+
+const SHORT_DELIMITER = Array.from({ length: 30 }, () => '~').join('');
+const LONG_DELIMITER = Array.from({ length: 30 }, () => '🔼').join('');
 
 /**
- * @param  {(string | Error)[]} args
+ * @param {...any} args
+ * @returns {string}
+ */
+const WrapForOutput = (...args) =>
+  args.map((arg) => inspect(arg, { depth: Infinity, colors: true })).join(`\n${SHORT_DELIMITER}\n`);
+
+/**
+ * @param  {...(string | Error)} args
  * @returns {void}
  */
 const LogMessageOrError = (...args) => {
@@ -11,12 +22,13 @@ const LogMessageOrError = (...args) => {
     (message) => message instanceof Error || (typeof message === 'string' && /test/i.test(message))
   );
   const out = containsError ? console.error : console.log;
+  const wrapped = WrapForOutput(...args);
 
   out(new Date());
-  out(...args);
-  out(Array.from({ length: 30 }, () => '~').join(''));
+  out(wrapped);
+  out(LONG_DELIMITER);
 
-  if (DEV) writeFile('./out/logmessageorerror.json', JSON.stringify(args, false, '\t')).catch(console.warn);
+  if (IS_DEV) writeFile('./out/logmessageorerror.json', JSON.stringify(args, false, '\t')).catch(console.warn);
 };
 
 export default LogMessageOrError;
